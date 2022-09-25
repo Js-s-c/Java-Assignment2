a675860ba44f92dd94025020a110a03926727fb0
JS <112610266+Js-s-c@users.noreply.github.com>
2022-09-25 18:13:05 +0530

    Update a9.java

--- a9.java
+++ a9.java
@@ -1,24 +1,21 @@
 //Question 9) Write Program Demonstrating use of privete
 //Code:-
-class B 
-{   
-private void s()   
-{   
-System.out.println("Javatpoint");   
-}   
-}   
-public class D  
-{   
-public static void main(String args[])   
-{   
-B a = new B();    
-a.s();   
-}   
+class B
+{
+    protected void s()
+    {
+        System.out.println("Hey");
+    }
+}
+public class D extends B
+{
+    public static void main(String args[])
+    {
+        B a = new B();
+        a.s();
+    }
 }
 /*
 Output:- 
-D.java:13: error: s() has private access in B
-a.s();
- ^
-1 error
+Hey
 */
